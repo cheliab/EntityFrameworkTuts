3f82f8de4178ad3d746a106036d36a456c042235
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-02-03 00:42:17 +1000

    Поправлен контроллер Home и сделан вывод и ввод пользователей

--- EntityFrameworkTuts/Controllers/HomeController.cs
+++ EntityFrameworkTuts/Controllers/HomeController.cs
@@ -6,32 +6,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EntityFrameworkTuts.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameworkTuts.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private ApplicationContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationContext db)
         {
-            _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Users.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
         {
-            return View();
-        }
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+            return RedirectToAction("Index");
         }
     }
 }
