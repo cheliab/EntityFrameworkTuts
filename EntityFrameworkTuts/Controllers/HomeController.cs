e543329ce815c80ee06b4d44c2f0a0d54eb0fd26
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-02-03 23:46:26 +1000

    Добавлены страницы просмотра, редактирования, удаления

--- EntityFrameworkTuts/Controllers/HomeController.cs
+++ EntityFrameworkTuts/Controllers/HomeController.cs
@@ -38,5 +38,69 @@ namespace EntityFrameworkTuts.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            User foundUser = await _db.Users.FirstOrDefaultAsync(user => user.Id == id);
+            if (foundUser == null)
+                return NotFound();
+
+            return View(foundUser);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            User foundUser = await _db.Users.FirstOrDefaultAsync(user => user.Id == id);
+            if (foundUser == null)
+                return NotFound();
+
+            return View(foundUser);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            _db.Users.Update(user);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfimDelete(int? id)
+        {
+            if (id == null)
+                NotFound();
+
+            User foundUser = await _db.Users.FirstOrDefaultAsync(user => user.Id == id);
+            if (foundUser == null)
+                NotFound();
+
+            return View(foundUser);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                NotFound();
+
+            User foundUser = await _db.Users.FirstOrDefaultAsync(user => user.Id == id);
+            if (foundUser == null)
+                NotFound();
+
+            _db.Users.Remove(foundUser);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
     }
 }
